31a88942c37a76562047de63f64f04ceea6ba581
enwumeh <54593478+enwumeh@users.noreply.github.com>
2020-08-20 09:12:51 -0500

    still working

--- app.js
+++ app.js
@@ -2,16 +2,44 @@
 // https://api.usa.gov/crime/fbi/sapi/a/api/summarized/estimates/states/{stateAbbr}/{since}/{until}?API_KEY=fqaOG7nblqnvp0dz1Ag2NHZ1sciWe7zipRByMW97?fqaOG7nblqnvp0dz1Ag2NHZ1sciWe7zipRByMW97
 
 
-// console.log(baseUrl)
+//getting everything from the DOM
+const natRangeFrom = document.querySelector('#nat-range-from')
+const natRangeTo = document.querySelector('#nat-range-to')
 const API_KEY = 'fqaOG7nblqnvp0dz1Ag2NHZ1sciWe7zipRByMW97'
+const stateSelect = document.querySelector('#state-select-crime')
+const selectRangeFrom = document.querySelector('#state-range-from')
+const selectRangeTo = document.querySelector('#state-range-to')
+const getDataButton = document.querySelector('#get-data-button')
+const stateDataDiv = document.querySelector('.state-data')
+const pageFormat = document.querySelector('.page-format')
 
+
+// console.log(stateSelect)
+///STATE NAME 
+
+
+//states drop down menu, looping through each state
+function stateOptionValues(list) {
+  list.forEach((stateObj) => {
+    
+    let stateValue = document.createElement('option')
+    stateValue.value = stateObj.state_abbr
+    stateValue.text = stateObj.state_name
+    stateSelect.appendChild(stateValue)
+    
+  })
+}
+
+
+//making axios request and getting list of states from api
 const getFBIdata = async () => {
-  const statesBaseUrl = `https://api.usa.gov/crime/fbi/sapi/api/states?API_KEY=${API_KEY}`;
-  console.log(statesBaseUrl)
+  const statesBaseUrl = `https://api.usa.gov/crime/fbi/sapi/api/states?API_KEY=${API_KEY}`
 
   try {
     const response = await axios.get(statesBaseUrl)
-    optionValues(response.data.results)
+   
+    // console.log(response.data.results)
+    stateOptionValues(response.data.results)
 
   } catch (error) {
     console.log(`Error logging: ${error}`)
@@ -19,32 +47,149 @@ const getFBIdata = async () => {
 }
 getFBIdata()
 
-function optionValues(list) {
-  list.forEach((stateAbbr) => {
-    const select = document.querySelector('#stateselectcrime')
-    const stateValue = document.createElement('option')
-    stateValue.value = stateAbbr.state_name
-    stateValue.text = stateAbbr.state_name
-    // console.log(stateValue)
-    select.append(stateValue)
-    select.addEventListener('change', () => {
-      console.log(select.value)
-      getState(select.value)
-    })
+
+//////////STATE YEAR//////////
+
+
+//stateOptionYearsFrom and stateOptionYearsTo append the state values to the drop down menu
+function stateOptionYearsFrom(list) {
+  list.forEach((stateObj) => {
+    // console.log(stateObj.year)
+    let stateValue = document.createElement('option')
+    stateValue.value = stateObj.year
+    stateValue.text = stateObj.year
+    selectRangeFrom.append(stateValue)
+    
+  })
+}
+function stateOptionYearsTo(list) {
+  list.forEach((stateObj) => {
+    // console.log(stateObj.year)
+    let stateValue = document.createElement('option')
+    stateValue.value = stateObj.year
+    stateValue.text = stateObj.year
+    selectRangeTo.append(stateValue)
+
+   
   })
 }
 
-const getState = async (stateName) => {
-  const chooseStateUrl = `https://api.usa.gov/crime/fbi/sapi/a/api/summarized/estimates/states/${stateName}/{since}/{until}?API_KEY=fqaOG7nblqnvp0dz1Ag2NHZ1sciWe7zipRByMW97?fqaOG7nblqnvp0dz1Ag2NHZ1sciWe7zipRByMW97`;
-  console.log(chooseStateUrl)
+//making axios request for 
+const getYearData = async () => {
+  const API_KEY = 'fqaOG7nblqnvp0dz1Ag2NHZ1sciWe7zipRByMW97'
+  const yearsBaseUrl = `https://api.usa.gov/crime/fbi/sapi/api/estimates/national/1985/2018?API_KEY=${API_KEY}`;
 
   try {
-    const response = await axios.get(chooseStateUrl)
+    const response = await axios.get(yearsBaseUrl)
+    stateOptionYearsFrom(response.data.results)
+    stateOptionYearsTo(response.data.results)
 
   } catch (error) {
     console.log(`Error logging: ${error}`)
   }
 }
+getYearData()
+
+
+
+
+/////GET STATE DATA
+const getStateData = async (stateInfo) => {
+  const chooseStateUrl = `https://api.usa.gov/crime/fbi/sapi/api/summarized/estimates/states/${stateInfo.stateName}/${stateInfo.stateFrom}/${stateInfo.stateTo}?API_KEY=fqaOG7nblqnvp0dz1Ag2NHZ1sciWe7zipRByMW97`;
+  console.log(chooseStateUrl)
+
+  try {
+    const response = await axios.get(chooseStateUrl)
+    let data = response.data.results
+    // console.log(response.data.results)
+    for (let i = 0; i < data.length; i++){
+      let dataDiv = document.createElement('div')
+      dataDiv.className = "data-div"
+      // dataDiv.append()
+      // console.log(response.data.results[i])
+      for (item in data[i]) {
+        console.log(item, data[i][item])
+      }
+    }
+    //loop through data and append data to stateDataDiv??
+
+
+  } catch (error) {
+    console.log(`Error logging: ${error}`)
+  }
+}
+
+// function stateScreen(breed)
+// const para = document.createElement('div')
+
+
+/////GET SELECT CHOICE
+
+let stateData={stateName:'', stateFrom:'', stateTo:''}
+stateSelect.addEventListener('change', ()=> {
+  console.log(stateSelect.value)
+  stateData.stateName = stateSelect.value
+  console.log('here',stateSelect)
+})
+
+selectRangeFrom.addEventListener('change', ()=>{
+  console.log(selectRangeFrom.value)
+  stateData.stateFrom = selectRangeFrom.value
+})
+selectRangeTo.addEventListener('change', ()=>{
+  console.log(selectRangeTo.value)
+  stateData.stateTo = selectRangeTo.value
+})
+
+getDataButton.addEventListener('click', (e)=>{
+  e.preventDefault()
+  console.log(stateData)
+  getStateData(stateData)
+ 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function getValues(e) {
@@ -58,27 +203,10 @@ const form = document.querySelector('form')
 form.addEventListener('submit', getValues)
 
 
-const select = document.querySelector('#natrangefrom')
-const select2 = document.querySelector('#natrangeto')
-const select3 = document.querySelector('#staterangefrom').addEventListener('change', () => {
-console.log(select2)
-})
-const select4 = document.querySelector('#staterangeto')
-
-const getYearData = async () => {
-  const API_KEY = 'fqaOG7nblqnvp0dz1Ag2NHZ1sciWe7zipRByMW97'
-  const yearsBaseUrl = `https://api.usa.gov/crime/fbi/sapi/api/estimates/national/1985/2018?API_KEY=${API_KEY}`;
 
 
-  try {
-    const response = await axios.get(yearsBaseUrl)
-    optionValues2(response.data.results)
 
-  } catch (error) {
-    console.log(`Error logging: ${error}`)
-  }
-}
-getYearData()
+// getYearData()
 
 // const select = document.querySelector('#natrangefrom')
 // const select2 = document.querySelector('#natrangeto')
@@ -87,27 +215,27 @@ getYearData()
 // })
 // const select4 = document.querySelector('#staterangeto')
 
-function optionValues2(list) {
-  list.forEach((yearElement) => {
-    const yearValue = document.createElement('option')
-    const yearValue2 = document.createElement('option')
-    const yearValue3 = document.createElement('option')
-    const yearValue4 = document.createElement('option')
-    yearValue.value = yearElement.year
-    yearValue.text = yearElement.year
-    yearValue2.value = yearElement.year
-    yearValue2.text = yearElement.year
-    yearValue3.value = yearElement.year
-    yearValue3.text = yearElement.year
-    yearValue4.value = yearElement.year
-    yearValue4.text = yearElement.year
-    select.append(yearValue)
-    select2.append(yearValue2)
-    select3.append(yearValue3)
-    select4.append(yearValue4)
-  })
+// function optionValues2(list) {
+//   list.forEach((yearElement) => {
+//     const yearValue = document.createElement('option')
+//     const yearValue2 = document.createElement('option')
+//     const yearValue3 = document.createElement('option')
+//     const yearValue4 = document.createElement('option')
+//     yearValue.value = yearElement.year
+//     yearValue.text = yearElement.year
+//     yearValue2.value = yearElement.year
+//     yearValue2.text = yearElement.year
+//     yearValue3.value = yearElement.year
+//     yearValue3.text = yearElement.year
+//     yearValue4.value = yearElement.year
+//     yearValue4.text = yearElement.year
+//     select.append(yearValue)
+//     // select2.append(yearValue2)
+//     select3.append(yearValue3)
+//     select4.append(yearValue4)
+//   })
 
-}
+// }
 
 function getValues2(e) {
   e.preventDefault()
@@ -137,3 +265,4 @@ async function postData(since, until) {
 
 // const form2 = document.querySelector('form')
 // form2.addEventListener('submit', getValues2)
+
